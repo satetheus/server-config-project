5c68ea6cd6edc5d388ddf4fdd0b7902ccd001b52
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-30 18:27:32 -0600

    added permissions & corrected syntax

--- configFile.sh
+++ configFile.sh
@@ -12,15 +12,21 @@ sudo adduser --disabled-password --gecos "" grader --quiet
 # sudo passwd -e chris
 # sudo passwd -e grader
 
-# add sudo capability to users
+# add sudo capability to chris
 sudo touch /etc/sudoers.d/chris
+sudo chmod 777 /etc/sudoers.d/chris
 sudo sed -i "vagrant ALL=(ALL) NOPASSWD:ALL" /etc/sudoers.d/chris
+sudo chmod 440 /etc/sudoers.d/chris
+
+# add sudo capability to grader
 sudo touch /etc/sudoers.d/grader
+sudo chmod 777 /etc/sudoers.d/grader
 sudo sed -i "vagrant ALL=(ALL) NOPASSWD:ALL" /etc/sudoers.d/grader
+sudo chmod 440 /etc/sudoers.d/grader
 
 # setup & set permissions for authorized keys
 sudo mkdir /.ssh && sudo touch /.ssh/authorized_keys
-
+sudo chmod 777 .ssh && sudo chmod 777 /.ssh/authorized_keys
 sudo sed -i "$a ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILMsIXqGZHeRoIpHVbWSbHkA6SO9zqv3hN05t10ah5E9 user@host" /.ssh/authorized_keys 
 sudo sed -i "$a ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLd9VUb4oo42+5FQoXTdEY3a7OfwsBIZTFM9NZCqq3g user@host" /.ssh/authorized_keys 
 sudo chmod 700 .ssh && sudo chmod 644 /.ssh/authorized_keys
@@ -37,7 +43,9 @@ ufw allow 2222/tcp
 ufw allow www
 
 # turn on firewall
-ufw enable -y
+ufw --force enable
+
+sudo chmod 777 /etc/ssh/sshd_config
 
 # disable root ssh login
 sudo sed -i "/^PermitRootLogin/s/yes/no/" /etc/ssh/sshd_config
@@ -51,11 +59,13 @@ sudo sed -i "/^Port/s/22/2222/" /etc/ssh/sshd_config
 # allow ssh from newly added users
 sudo sed -i "$a AllowUsers chris grader" /etc/ssh/sshd_config
 
+sudo chmod 644 /etc/ssh/sshd_config
+
 # install apache
-sudo apt-get install apache2 apache2-bin
+sudo apt-get install apache2 apache2-bin -y
 
 # install mod-wsgi
-sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install libapache2-mod-wsgi -y
 
 # download catalog project
 sudo git clone https://github.com/satetheus/CatalogProject /var/www/html/catalog
